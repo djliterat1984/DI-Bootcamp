adbf3fe12398d5eeac9acfa25c55e9969ceae770
djliterat1984 <50715680+djliterat1984@users.noreply.github.com>
2024-08-16 13:15:18 +0300

    resolve ex1

--- Week1/Day5/ExcerciseXP/exerciseXP+.py
+++ Week1/Day5/ExcerciseXP/exerciseXP+.py
@@ -2,30 +2,61 @@
 # Instructions
 # You are given a dictionary containing student names as keys and lists of their grades as values. Your task is to create a summary report that calculates the average grade for each student, assigns a letter grade, and determines the class average.
 
-
-
 # Initial Data:
 
-
-# student_grades = {
-#     "Alice": [88, 92, 100],
-#     "Bob": [75, 78, 80],
-#     "Charlie": [92, 90, 85],
-#     "Dana": [83, 88, 92],
-#     "Eli": [78, 80, 72]
-# }
+student_grades = {
+    "Alice": [88, 92, 100],
+    "Bob": [75, 78, 80],
+    "Charlie": [92, 90, 85],
+    "Dana": [83, 88, 92],
+    "Eli": [78, 80, 72]
+}
 
 
 # Requirements:
-# Calculate the average grade for each student and store the results in a new dictionary called student_averages.
-# Assign each student a letter grade (A, B, C, D, F) based on their average grade according to the following scale, and store the results in a dictionary called student_letter_grades:
+# 1.Calculate the average grade for each student and store the results in a new dictionary called student_averages.
+student_averages = {}
+for student, value in student_grades.items():
+	average = sum(value)/len(value)
+	student_averages.update({student: average})
+
+print(student_averages)
+
+# 2.Assign each student a letter grade (A, B, C, D, F) based on their average grade according to the following scale, and store the results in a dictionary called student_letter_grades:
 # A: 90 and above
 # B: 80 to 89
 # C: 70 to 79
 # D: 60 to 69
 # F: Below 60
-# Calculate the class average (the average of all students’ averages) and print it.
-# Print the name of each student, their average grade, and their letter grade.
+student_letter_grades = {}
+for key, value in student_averages.items():
+	letter_grade = ""
+	if value >= 90:
+		letter_grade = "A"
+	elif 80 < value <= 89:
+		letter_grade = "B"
+	elif 70 < value <= 79:
+		letter_grade = "C"
+	elif 60 < value <= 69:
+		letter_grade = "D"
+	elif value < 60:
+		letter_grade = "F"
+	
+	student_letter_grades.update({key: letter_grade})
+
+print(student_letter_grades)
+
+# 3.Calculate the class average (the average of all students’ averages) and print it.
+
+class_average = sum(student_averages.values())/len(student_averages)
+print(class_average)
+
+# 4.Print the name of each student, their average grade, and their letter grade.
+
+for key,value in student_averages.items():
+	print(f"Name: {key} - Average grade: {value} - Letter grade: {student_letter_grades[key]}")
+
+
 # Hints:
 # Use loops to iterate through the student_grades dictionary.
 # You may use sum() and len() functions to help calculate averages.
