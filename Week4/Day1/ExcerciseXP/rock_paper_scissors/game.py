2e8127ad889e0b671dbce9395fe55c3ad04c5bd6
djliterat1984 <50715680+djliterat1984@users.noreply.github.com>
2024-08-26 08:46:46 +0300

    resolve exercise rock papaer and scissors

---
+++ Week4/Day1/ExcerciseXP/rock_paper_scissors/game.py
@@ -0,0 +1,66 @@
+import random
+
+
+class Game:
+	results = {'won':0,'lost':0,'drew':0}
+
+	def __init__(self):
+		pass
+	
+	def is_valid(self,item):
+		return item == 'r' or item == 'p' or item == 's'
+
+	def get_user_item(self):
+		is_valid = False
+		user_selection = ''
+		while is_valid == False:
+			user_selection = input("Select (r)ock/(p)aper/(s)cissors: ")
+			is_valid = self.is_valid(user_selection.lower())
+		
+		return user_selection
+	
+	def get_computer_item(self):
+		options = ['r','s','p']
+		return random.choice(options)
+	
+	def get_game_result(self, user_item, computer_item):
+		if user_item == computer_item:
+			Game.results['drew'] += 1
+			return 'draw'
+		elif user_item == 'r' and computer_item == 's':
+			Game.results['won'] += 1
+			return 'win'
+		elif user_item == 'r' and computer_item == 'p':
+			Game.results['lost'] += 1
+			return 'lose'
+		elif user_item == 's' and computer_item == 'r':
+			Game.results['lost'] += 1
+			return 'lose'
+		elif user_item == 's' and computer_item == 'p':
+			Game.results['won'] += 1
+			return 'win'
+		elif user_item == 'p' and computer_item == 'r':
+			Game.results['won'] += 1
+			return 'win'
+		elif user_item == 'p' and computer_item == 's':
+			Game.results['lost'] += 1
+			return 'lose'
+		
+	def play(self):
+		user_selection = self.get_user_item()
+		user_selected_name = self.get_name_selection(user_selection)
+		computer_selection = self.get_computer_item()
+		computer_selected_name = self.get_name_selection(computer_selection)
+		result = self.get_game_result(user_selection,computer_selection)
+		print(f'You selected {user_selected_name}. The computer selected {computer_selected_name}. You {result}.')
+
+	def get_name_selection(self,item):
+		if item == 'r':
+			return 'rock'
+		elif item == 's':
+			return 'scissors'
+		elif item == 'p':
+			return 'paper'
+
+# game = Game()
+# game.play()
---
+++ Week4/Day1/ExcerciseXP/rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,31 @@
+from game import Game
+
+def get_user_menu_choice():
+	print("MENU")
+	print('(g) Play a new game')
+	print('(x) Show scores and exit')
+
+	user_selection = input('Select an option: ')
+	return user_selection.lower()
+	
+def print_results(results):
+	print('Game Results')
+	print(f'You won {results['won']} times')
+	print(f'You lost {results['lost']} times')
+	print(f'You drew {results['drew']} times')
+		
+
+def main():
+	is_playing = True
+	while is_playing:
+		user_selection = get_user_menu_choice()
+		game = Game()
+		if user_selection == 'g':
+			game.play()
+		elif user_selection == 'x' or  user_selection == 'q':
+			print_results(game.results)
+			is_playing = False
+		else:
+			print("Select a valid option.")
+
+main()
