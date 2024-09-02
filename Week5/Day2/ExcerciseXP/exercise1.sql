b652c3f505103b9fd37ecadb940fa9fb7e25c032
djliterat1984 <50715680+djliterat1984@users.noreply.github.com>
2024-09-02 21:41:55 +0300

    add instructions exercise xp/+

--- Week5/Day2/ExcerciseXP/exercise1.sql
+++ Week5/Day2/ExcerciseXP/exercise1.sql
@@ -37,3 +37,33 @@ SELECT * FROM customers WHERE last_name = 'Smith';
 SELECT * FROM customers WHERE last_name = 'Jones';
 
 SELECT * FROM customers WHERE last_name != 'Scott';
+
+-- Exercise 1 : Items and customers
+-- Create a database called public.
+-- Add two tables:
+-- items
+-- customers.
+
+
+-- Follow the instructions below to determine which columns and data types to add to the two tables:
+
+-- Add the following items to the items table:
+-- 1 - Small Desk – 100 (ie. price)
+-- 2 - Large desk – 300
+-- 3 - Fan – 80
+
+-- Add 5 new customers to the customers table:
+-- 1 - Greg - Jones
+-- 2 - Sandra - Jones
+-- 3 - Scott - Scott
+-- 4 - Trevor - Green
+-- 5 - Melanie - Johnson
+
+-- Use SQL to fetch the following data from the database:
+-- All the items.
+-- All the items with a price above 80 (80 not included).
+-- All the items with a price below 300. (300 included)
+-- All customers whose last name is ‘Smith’ (What will be your outcome?).
+-- All customers whose last name is ‘Jones’.
+-- All customers whose firstname is not ‘Scott’.
+
