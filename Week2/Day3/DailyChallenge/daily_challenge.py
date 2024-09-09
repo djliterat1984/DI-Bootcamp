8d6942e2ac5541c0dcd4ac14d4b4ad5a55a72342
djliterat1984 <50715680+djliterat1984@users.noreply.github.com>
2024-09-09 12:27:50 +0300

    resolve challenge 2-2

--- Week2/Day3/DailyChallenge/daily_challenge.py
+++ Week2/Day3/DailyChallenge/daily_challenge.py
@@ -1,13 +1,24 @@
 # Instructions
 # Here is a python code that generates a list of 20000 random numbers, called list_of_numbers, and a target number.
 
-# import random
+import random
 
-# list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
+list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 
-# target_number   = 3728
+target_number   = 3728
 
+pairs_sum = []
 
+for i in list_of_numbers:
+	if i > target_number:
+		continue
+	
+	num2 = target_number - i
+	if num2 in list_of_numbers:
+		pairs_sum.append((num2,i))
+
+final_list = set(pairs_sum)
+print(final_list)
 # Copy this code, and create a program that finds, within list_of_numbers all the pairs of number that sum to the target number
 
 # For example
