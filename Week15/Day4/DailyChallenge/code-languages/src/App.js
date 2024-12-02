1f65c8dec4b4a7d326c2fd3c0ba785bb519fc6bc
djliterat1984 <50715680+djliterat1984@users.noreply.github.com>
2024-12-02 21:17:41 +0200

    finish daily challenge week 15 day 4

--- Week15/Day4/DailyChallenge/code-languages/src/App.js
+++ Week15/Day4/DailyChallenge/code-languages/src/App.js
@@ -1,3 +1,16 @@
+// Steps
+// In the App.js create an array of objects in the state. Each object contains the language and the number of votes per language.
+// Use the below object as a starting point:
+
+// const [languages, setLanguages] = useState([
+//                                             {name: "Php", votes: 0},
+//                                             {name: "Python", votes: 0},
+//                                             {name: "JavaSript", votes: 0},
+//                                             {name: "Java", votes: 0}
+                                          // ])
+
+// Create a function that increases the state of the votes by one, when you click on a specific language button.
+
 import { useState } from 'react';
 import './App.css';
 import LanguageComponent from './components/LanguageComponent';
